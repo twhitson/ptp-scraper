b27cb05499b4b4483e22cfb482141115a25acdb4
Trey Whitson <trey.whitson@gmail.com>
2018-03-19 15:58:28 -0500

    Move from puppeteer to request

--- index.js
+++ index.js
@@ -1,65 +1,91 @@
-const puppeteer = require('puppeteer');
-const axios = require('axios');
-const config = require('./config.json');
-const fs = require('fs');
+const request = require('request').defaults({ jar: true })
+const config = require('./config.json')
+const fs = require('fs')
 
-(async () => {
-  const browser = await puppeteer.launch({
-    args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+let grabbed = JSON.parse(fs.readFileSync('grabbed.json'))
+let embeds = []
+let pageData = {}
+
+login()
+
+function login() {
+  request({
+    method: 'POST',
+    uri: 'https://passthepopcorn.me/ajax.php?action=login',
+    form: {
+      username: config.username,
+      password: config.password,
+      passkey: config.passkey,
+      keeplogged: '0',
+      login: 'Login In!'
+    }
+  }, (err, res, body) => {
+    if (err) return console.error(err)
+
+    getPageData()
   })
-  let grabbed = JSON.parse(fs.readFileSync('grabbed.json'))
-  let embeds = []
+}
 
-  const page = await browser.newPage()
+function getPageData() {
+  request({
+    uri: 'https://passthepopcorn.me/torrents.php?freetorrent=1&grouping=0&json=noredirect'
+  }, (err, res, body) => {
+    if (err) return console.error(err)
 
-  await page.setUserAgent(config.useragent)
+    try {
+      pageData = JSON.parse(body)
+    } catch (e) {
+      return console.warn('PageData is not JSON. Are your credentials correct?')
+    }
 
-  await page.goto('https://passthepopcorn.me')
+    findGoodTorrents()
+  })
+}
 
-  await page.type('#username', config.username)
-  await page.type('#password', config.password)
+function findGoodTorrents() {
+  if (!pageData.Movies) return console.warn('PageData empty')
 
-  await page.click('#login-button')
-  await page.waitForNavigation()
-
-  await page.goto(config.url)
-  
-  let pageData = await page.evaluate(() => PageData)
   pageData.Movies.forEach(movie => {
-    movie.GroupingQualities.forEach(quality => {
-      quality.Torrents.forEach(torrent => {
-        if (grabbed.grabbed.includes(torrent.TorrentId)) return;
+    movie.Torrents.forEach(torrent => {
+      if (grabbed.grabbed.includes(torrent.TorrentId)) return;
 
-        if (parseInt(torrent.Leechers) > 10) {
+      if (parseInt(torrent.Leechers) > 10) {
 
-          embeds.push({
-            title: torrent.ReleaseName,
-            description: movie.Title + '\n' + torrent.Title.replace(/<\/?[^>]+(>|$)/g, "").replace("&#9745;", "").trim(),
-            url: 'https://passthepopcorn.me/torrents.php?id=' + movie.GroupId + '&torrentid=' + torrent.TorrentId,
-            fields: [
-              { name: 'Size', value: torrent.Size, inline: true },
-              { name: 'Snatches', value: torrent.Snatched, inline: true },
-              { name: 'Seeders', value: torrent.Seeders, inline: true },
-              { name: 'Leechers', value: torrent.Leechers, inline: true }
-            ]
-          })
+        embeds.push({
+          title: torrent.ReleaseName,
+          description: `${movie.Title} (${movie.Year})`,
+          url: 'https://passthepopcorn.me/torrents.php?id=' + movie.GroupId + '&torrentid=' + torrent.TorrentId,
+          fields: [
+            { name: 'Size', value: formatBytes(torrent.Size), inline: true },
+            { name: 'Snatches', value: torrent.Snatched, inline: true },
+            { name: 'Seeders', value: torrent.Seeders, inline: true },
+            { name: 'Leechers', value: torrent.Leechers, inline: true }
+          ]
+        })
 
-          grabbed.grabbed.push(torrent.TorrentId)
-        }
-      })
+        grabbed.grabbed.push(torrent.TorrentId)
+      }
     })
   })
 
-  await browser.close()
+  sendWebhook()
+}
 
-  if (embeds.length === 0) return;
+function sendWebhook() {
+  if (embeds.length === 0) return
 
-  axios.post(config.webhook, {
-    content: '',
-    embeds: embeds
+  request({
+    uri: config.webhook,
+    method: 'POST',
+    json: {
+      content: '',
+      embeds: embeds
+    }
+  }, (err, res, body) => {
+    if (err) return console.error(err)
   })
-  .then(res => { console.log(embeds) })
-  .catch(err => { console.error(err) })
 
   fs.writeFileSync('grabbed.json', JSON.stringify(grabbed))
-})()
+}
+
+function formatBytes(a, b) { if (0 == a) return "0 Bytes"; var c = 1024, d = b || 2, e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"], f = Math.floor(Math.log(a) / Math.log(c)); return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f] }
