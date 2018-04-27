bb1129509075da86c925c29be6bb799ad928c438
Trey Whitson <trey.whitson@gmail.com>
2018-04-27 12:10:33 -0500

    Syntax fix

--- index.js
+++ index.js
@@ -185,6 +185,7 @@ function uploadTorrents() {
       username: config.sftp.username,
       password: config.sftp.password
     })
-  }
+  })
+}
 
 function formatBytes(a, b) { if (0 == a) return "0 Bytes"; var c = 1024, d = b || 2, e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"], f = Math.floor(Math.log(a) / Math.log(c)); return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f] }
